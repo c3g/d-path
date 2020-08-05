35cbc3ad8300e4150976ea840bdf757ee8283422
Rom Grk <romgrk.cc@gmail.com>
2020-08-05 17:33:43 -0400

    Landing: improve styling

--- src/Landing.js
+++ src/Landing.js
@@ -9,17 +9,19 @@ class Landing extends Component {
       <Container>
         <div>
           <Jumbotron>
-            <h1>Welcome to the D-Path Tool</h1>
-            <p>
+            <h1 className='text-center' style={{ fontSize: '45px', marginBottom: '1em' }}>
+              Welcome to the D-Path Tool
+            </h1>
+            <p style={{ fontSize: '20px', margin: '0 auto 3em', width: '80ch', maxWidth: '100%' }}>
               D-PATH (Data Privacy Assessment Tool for Health) is a privacy assessment tool for the sharing of health-related data
               that aims to guide researchers, data hosts, and service providers such as cloud computing companies, in protecting the privacy and confidentiality
               of the human health-related datasets they process in a manner that is in agreement with the applicable legal, professional and ethical norms.
             </p>
-            <p>
-              <Link to="/start">
-                <Button variant="primary"> Start  </Button>
+            <div className='text-center'>
+              <Link to='/start' className='btn btn-primary btn-lg'>
+                Start My Assessment
               </Link>
-            </p>
+            </div>
           </Jumbotron>
         </div>
       </Container>

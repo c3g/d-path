eb6967f129e96dcb6b1c0eb6acbe41f8e1343c5e
Rom Grk <romgrk.cc@gmail.com>
2020-08-05 19:26:19 -0400

    PersonalInformation: fix styling issues & refactor

--- src/forms/PersonalInfo.js
+++ src/forms/PersonalInfo.js
@@ -27,53 +27,58 @@ class PersonalInfo extends Component{
 
     getIdentifiableForm = () => {
       return (
-        <div>
-          <h1> Is it personal information? </h1>
-          <Alert variant='info' style={{paddingBottom: '1%'}}> Locations selected: {this.props.locations.join(', ')}</Alert>
-          <h4 style={{paddingBottom: '1%'}}> Is the information
-           <OverlayTrigger trigger='hover' placement='top' overlay={directly}>
-              <Alert.Link> Directly </Alert.Link>
-            </OverlayTrigger> or
-             <OverlayTrigger trigger='hover' placement='top' overlay={indirectly}>
-              <Alert.Link> Indirectly </Alert.Link>
-             </OverlayTrigger>
-            Identifiable? </h4>
+        <>
+          <h4 style={{paddingBottom: '1%'}}>
+            Is the information{' '}
+            <OverlayTrigger trigger='hover' placement='top' overlay={directly}>
+              <abbr>directly</abbr>
+            </OverlayTrigger>{' '}
+            or{' '}
+            <OverlayTrigger trigger='hover' placement='top' overlay={indirectly}>
+              <abbr>indirectly</abbr>
+            </OverlayTrigger>{' '}
+            identifiable?
+          </h4>
           <ButtonGroup style={{width:'100%'}} size='lg' vertical>
             <Button variant='light' onClick={(e) => this.saveIdentifiable(true)}>Yes</Button>
             <Button variant='light' onClick={(e) => this.saveAndContinue(e, false) }>No</Button>
           </ButtonGroup>
-        </div>
+        </>
       );
     }
 
     getPublicForm = () => {
       return (
-        <div>
-          <h1> Is it personal information? </h1>
-          <Alert variant='info' style={{paddingBottom: '1%'}}> Locations selected: {this.props.locations.toString()}</Alert>
-          <h4 style={{paddingBottom: '1%'}}> Is the information
-           <OverlayTrigger trigger='hover' placement='top' overlay={publicInfo}>
-           <Alert.Link> Public </Alert.Link>
-           </OverlayTrigger> ?
+        <>
+          <h4 style={{paddingBottom: '1%'}}>
+            Is the information{' '}
+            <OverlayTrigger trigger='hover' placement='top' overlay={publicInfo}>
+              <abbr>public</abbr>
+            </OverlayTrigger> ?
           </h4>
           <ButtonGroup style={{width:'100%'}} size='lg' vertical>
             <Button variant='light' onClick={(e) => this.saveAndContinue(e, false )}>Yes</Button>
             <Button variant='light' onClick={(e) => this.saveAndContinue(e, true )}>No</Button>
           </ButtonGroup>
-          <div style={{paddingTop: '3%'}}>
-            <Button variant={'warning'} onClick={(e) => this.back(e)}> Back </Button>
-            <Link to='/'>
-              <span className='restartButton'> Restart </span>
-            </Link>
-          </div>
-        </div>
+        </>
       );
     }
 
     render(){
         const { identifiable } = this.state;
-        const { values } = this.props;
-        return (identifiable) ? this.getPublicForm() : this.getIdentifiableForm();
+
+        return (
+          <div>
+            <h1>Personal information</h1>
+            <Alert variant='info' style={{paddingBottom: '1%'}}>
+              Locations selected: {this.props.locations.join(', ')}
+            </Alert>
+            {identifiable ?
+              this.getPublicForm() :
+              this.getIdentifiableForm()
+            }
+          </div>
+        )
     }
 }
 

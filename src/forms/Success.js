314781228725976b2461a0a60d4204a13a94a78d
Sebastian Ballesteros Ramirez <sballesteros@Strelitzia.local>
2020-08-06 15:04:53 -0400

    Improved the success component

--- src/forms/Success.js
+++ src/forms/Success.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 
 class Success extends Component{
@@ -12,36 +13,39 @@ class Success extends Component{
         const {answers, userType, isPersonalInfo } = this.props;
         return(
             <div>
-                <h1 className='ui centered'> Summary </h1>
-                <div style={{paddingTop: '3%'}}>
-                <h4> Type of User: </h4>
-                <p> { userType } </p>
-                <h4> Locations selected :</h4>
-                <div>
-                    <h6>Where is the organization?</h6>
-                    <p>{answers.organization}</p>
-                </div>
-                <div>
-                    <h6>Where is the data processed?</h6>
-                    <p>{answers.dataProcessed}</p>
-                </div>
-                <div>
-                    <h6>Where are the data users?</h6>
-                    <p>{answers.dataUsers}</p>
-                </div>
-                <div>
-                    <h6>Where are the data donors?</h6>
-                    <p>{answers.dataDonors}</p>
-                </div>
-
-                <h4> Is it personal Information? </h4>
-                <p> { isPersonalInfo ? 'True' : 'False'}</p>
-                </div>
-                <div style={{paddingTop: '3%'}}>
-                  <Link className='displayButton' onClick={(e) => this.saveInformation(e)} to='/info'>
-                    Display Laws and Regulations
-                  </Link>
-                </div>
+              <h1 className='ui centered'> Summary </h1>
+                <Row className='summaryInfo'>
+                  <Col lg={6}>
+                    <h4> Locations selected :</h4>
+                    <div>
+                        <h6>Where is the organization?</h6>
+                        <p>{answers.organization}</p>
+                    </div>
+                    <div>
+                        <h6>Where is the data processed?</h6>
+                        <p>{answers.dataProcessed}</p>
+                    </div>
+                    <div>
+                        <h6>Where are the data users?</h6>
+                        <p>{answers.dataUsers}</p>
+                    </div>
+                    <div>
+                        <h6>Where are the data donors?</h6>
+                        <p>{answers.dataDonors}</p>
+                    </div>
+                  </Col>
+                  <Col lg={6}>
+                    <h4> Type of User: </h4>
+                    <p> { userType } </p>
+                    <h4> Is it personal Information? </h4>
+                    <p> { isPersonalInfo ? 'Yes' : 'No'}</p>
+                    <div style={{paddingTop: '3%'}}>
+                      <Link className='displayButton' onClick={(e) => this.saveInformation(e)} to='/info'>
+                        Display Laws and Regulations
+                      </Link>
+                    </div>
+                  </Col>
+                </Row>
             </div>
         )
     }

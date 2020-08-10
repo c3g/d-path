b5a093fb53c1a3fb7ef02d51d9bd8b5f7adc2018
Rom Grk <romgrk.cc@gmail.com>
2020-08-09 20:51:38 -0400

    index: add HMR

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import style from './stylesheets/style.css'; 
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
+import './stylesheets/style.css'; 
+
 
 ReactDOM.render(
     <BrowserRouter>
@@ -12,3 +13,17 @@ ReactDOM.render(
     </BrowserRouter>,
   document.getElementById('root')
 );
+
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App')
+
+    ReactDOM.render(
+        <BrowserRouter>
+          <NextApp />
+        </BrowserRouter>,
+      document.getElementById('root')
+    );
+  })
+}
